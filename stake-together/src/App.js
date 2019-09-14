53ac4a2c0cc0b313b2db958864b043f28c766153
czfloyd <floyd.c23@gmail.com>
2019-09-14 14:21:36 -0400

    Added button

--- stake-together/src/App.js
+++ stake-together/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import Search from './Search';
 import './App.css';
 
 function App() {
@@ -7,9 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Search></Search>
         <a
           className="App-link"
           href="https://reactjs.org"
