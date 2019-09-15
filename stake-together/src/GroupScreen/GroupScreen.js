cfdaa3c3d40aebdeacc65a1d24b1d8d36f131d7a
Myra Dotzel <myradotzel@gmail.com>
2019-09-14 19:24:10 -0500

    added rough GroupScreen

--- stake-together/src/GroupScreen/GroupScreen.js
+++ stake-together/src/GroupScreen/GroupScreen.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import Search from '../Search';
+import NavBar from '../NavBar';
+import { Box } from '@material-ui/core';
+import VirtualizedList from '../VirtualizedList';
 
 function GroupScreen (onPageChange) {
     return (
         <div style = {{margin: 'auto'}}>
-            Group
-            <Search/>
+
+            <div>
+                <div style={{width: '33.33%', float: 'left'}}>Number of Investors</div>
+                <div style={{width: '33.33%', float: 'left'}}>Amount Invested</div>
+                <div style={{width: '33.33%', float: 'left'}}>Amount left to invest</div>
+            </div>
+
+            <img></img>
+
+            <div style={{float: 'left', width: '50%', height: '30%'}}>Property Info</div>
+
+            <VirtualizedList/>
+
         </div>
     )
 }
