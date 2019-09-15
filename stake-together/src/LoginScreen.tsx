86bd5e72a41223bcc8060436f757ffaa5710b253
czfloyd <floyd.c23@gmail.com>
2019-09-15 02:42:19 -0400

    Added image

--- stake-together/src/LoginScreen.tsx
+++ stake-together/src/LoginScreen.tsx
@@ -97,25 +97,28 @@ export default function SignInSide(props: any) {
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
             />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
+                <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+                style={{width: '80%'}}
+                >
+                Register
+                </Button>
               </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
+              <Grid item xs>
+                <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+                style={{width: '80%'}}
+                >
+                Sign In
+                </Button>
               </Grid>
             </Grid>
             <Box mt={5}>
--- stake-together/src/NavBar.tsx
+++ stake-together/src/NavBar.tsx
@@ -5,7 +5,6 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import PageEnum from './PageEnum';
 
 const useStyles = makeStyles(theme => ({
@@ -28,9 +27,11 @@ export default function ButtonAppBar(props: any) {
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+            
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h6" className={classes.title} onClick={ () => {
+            props.onPageChange(PageEnum.HOME);
+          }}>
             Stake Together
           </Typography>
           <Button color="inherit" onClick={ () => {
